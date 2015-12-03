b2af1dd9407410834120bada534bd564d97b4ad9
J. B. Rainsberger <me@jbrains.ca>
2015-12-03 16:35:04 +0100

    We now ignore empty commands.

--- test/source/ca/jbrains/pos/test/ReadBarcodesFromAReaderTest.java
+++ test/source/ca/jbrains/pos/test/ReadBarcodesFromAReaderTest.java
@@ -50,13 +50,33 @@ public class ReadBarcodesFromAReaderTest {
                         "::barcode 4::"));
     }
 
+    @Test
+    public void ignoreEmptyLines() throws Exception {
+        context.checking(new Expectations() {{
+            oneOf(barcodeScannedListener).onBarcode(with("::barcode 1::"));
+            oneOf(barcodeScannedListener).onBarcode(with("::barcode 2::"));
+            oneOf(barcodeScannedListener).onBarcode(with("::barcode 3::"));
+            oneOf(barcodeScannedListener).onBarcode(with("::barcode 4::"));
+        }});
+
+        process(new StringReader(
+                "::barcode 1::\n\n" +
+                        "::barcode 2::\n\n\n\n\n" +
+                        "::barcode 3::\n\n\n" +
+                        "::barcode 4::\n\n\n\n\n\n\n\n\n"));
+    }
+
     private void process(Reader textCommandSource) throws IOException {
         final BufferedReader bufferedReader
                 = new BufferedReader(textCommandSource);
 
-        bufferedReader.lines().forEach(
-                (line) -> barcodeScannedListener.onBarcode(line)
-        );
+        bufferedReader.lines()
+                .filter(
+                        (line) -> !line.isEmpty()
+                )
+                .forEach(
+                        (line) -> barcodeScannedListener.onBarcode(line)
+                );
     }
 
     public interface BarcodeScannedListener {
