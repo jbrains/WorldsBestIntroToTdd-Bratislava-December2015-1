3b49adc026cb8d7a7c7743d36104e314d8a8dbdc
J. B. Rainsberger <me@jbrains.ca>
2015-12-03 14:05:05 +0100

    We now feel confident that when we find a price, it is the price that matches the barcode.

--- test/source/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
+++ test/source/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class FindPriceInMemoryCatalogTest {
@@ -14,6 +15,24 @@ public class FindPriceInMemoryCatalogTest {
 
         final InMemoryCatalog catalog = new InMemoryCatalog(
                 Collections.singletonMap("12345", price));
+
+        Assert.assertEquals(price, catalog.findPrice("12345"));
+    }
+
+    @Test
+    public void productFoundAmongOthers() throws Exception {
+        final Price price = Price.cents(1250);
+
+        final Map pricesByBarcode = new HashMap() {{
+            put("definitely not 12345", new Price());
+            put("not 12345", new Price());
+            put("12345", price);
+            put("anything in the world except 12345", new Price());
+        }};
+        // Checking a precondition
+        Assert.assertNotEquals("12345", pricesByBarcode.keySet().iterator().next());
+
+        final InMemoryCatalog catalog = new InMemoryCatalog(pricesByBarcode);
 
         Assert.assertEquals(price, catalog.findPrice("12345"));
     }
@@ -34,7 +53,10 @@ public class FindPriceInMemoryCatalogTest {
         }
 
         public Price findPrice(String barcode) {
-            return pricesByBarcode.get(barcode);
+            if (pricesByBarcode.containsKey(barcode))
+                return pricesByBarcode.get(barcode);
+            else
+                return null;
         }
     }
 }
