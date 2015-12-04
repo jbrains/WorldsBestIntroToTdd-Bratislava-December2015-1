ede41192b047350bb0b7e92d2a23c711ea13eef6
J. B. Rainsberger <me@jbrains.ca>
2015-12-04 07:48:59 +0100

    We can now scan barcodes using the barcode scanner.

--- production/source/ca/jbrains/pos/VirtualPointOfSaleTerminal.java
+++ production/source/ca/jbrains/pos/VirtualPointOfSaleTerminal.java
@@ -1,26 +1,29 @@
 package ca.jbrains.pos;
 
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.util.Collections;
 
 public class VirtualPointOfSaleTerminal {
     public static void main(String[] args) {
-        final SellOneItemController controller = new SellOneItemController(
-                new InMemoryCatalog(
-                        Collections.singletonMap("8586009260819", Price.cents(70))
-                ),
-                new Display() {
-                    @Override
-                    public void displayPrice(Price price) {
-                        System.out.println(String.format("EUR %.2f", price.euroValue()));
-                    }
+        final CommandProcessor commandProcessor = new CommandProcessor(
+                new SellOneItemController(
+                        new InMemoryCatalog(
+                                Collections.singletonMap("8586009260819", Price.cents(70))
+                        ),
+                        new Display() {
+                            @Override
+                            public void displayPrice(Price price) {
+                                System.out.println(String.format("EUR %.2f", price.euroValue()));
+                            }
 
-                    @Override
-                    public void displayProductNotFoundMessage(String barcodeNotFound) {
-                        System.out.println(String.format("Product not found for %s", barcodeNotFound));
-                    }
-                }
-        );
-        controller.onBarcode("12345");
-        controller.onBarcode("8586009260819");
+                            @Override
+                            public void displayProductNotFoundMessage(String barcodeNotFound) {
+                                System.out.println(String.format("Product not found for %s", barcodeNotFound));
+                            }
+                        }
+                ));
+
+        commandProcessor.processCommands(new InputStreamReader(System.in));
     }
 }
